55874d328f2b16ac0469d5d76390706b1fcb6e01
Roger Howard - WellSky <roger.howard@mediware.com>
2020-03-15 21:04:59 -0400

    Added Override RankedGradeBook method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,32 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            //public char letterGrade = " ";
+
+            if (Students.Count < 5)
+            {
+                throw InvalidOperationException("There are not enough students.");
+            }
+
+            //switch (averageGrade)
+            //{
+            //    case 1(averageGrade > 90):
+            //        return GetLetterGrade = "A";
+            //    default:
+            //        break;
+            //}
+            return base.GetLetterGrade(averageGrade);
+            //return averageGrade = "F";
+
+        }
+
+        private Exception InvalidOperationException(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
