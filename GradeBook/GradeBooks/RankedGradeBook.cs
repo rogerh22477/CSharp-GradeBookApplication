8b623db096af141c67d828979d298fe787e04b6f
Roger Howard <RogerH@mediware.com>
2020-03-20 22:27:54 -0400

    Added the CalculateStudentStatistics override method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -59,6 +59,17 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
         }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+
+            }
+            else
+                base.CalculateStudentStatistics(name);
+        }
+
         private Exception InvalidOperationException(string v)
         {
             throw new NotImplementedException();
